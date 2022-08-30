9a7c83a2dfcf338ab092ef65be8bd604c80961f0
Marc D Wagner <marc.wagner@yahoo.com>
2022-08-30 12:43:38 -0700

    chunk of js complete

--- apps.js
+++ apps.js
@@ -1,3 +1,48 @@
 'use strict';
 
+let productContainer = document.querySelector('section');
+let resultButton = document.querySelector('section + div');
+let product1 = document.querySelector('section img:first-child');
+let product2 = document.querySelector('section img:nth-child(2)');
+let product3 = document.querySelector('section img:nth-childa(3)');
 
+let selections = 0;
+let maxSelectionsAllowed = 25;
+let uniqueImageCount = 19;
+let indexArray = [];
+
+function Product(name, src) {
+  this.name = name;
+  this.src = src;
+  this.views = 0;
+  this.seclections = 0;
+  Product.allProductsArray.push(this);
+}
+
+Product.allProductsArray = [];
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * allProductsArray.length);
+}
+
+function showProducts() {
+  while (indexArray.length < uniqueImageCount) {
+    let randomNumber = getRandomNumber();
+    if (!indexArray.includes(randomNumber)) {
+      indexArray.push(randomNumber);
+    }
+  }
+  console.log(indexArray);
+
+  let image1 = indexArray.shift();
+  let image2 = indexArray.shift();
+  let image3 = indexArray.shift();
+  product1.src = Product.allProductsArray[image1].src;
+  product2.src = Product.allProductsArray[image2].src;
+  product3.src = Product.allProductsArray[image3].src;
+  product1.alt = Product.allProductsArray[image1].name;
+  product2.alt = Product.allProductsArray[image2].name;
+  product3.alt = Product.allProductsArray[image3].name;
+}
+
+function handleProductClick(event) {}
